9fcd76f83366122f230c4a9b9eb656b5f75f44e6
Yusuf Abdulhadi <61198189+abdul-yusuf@users.noreply.github.com>
2024-08-20 06:03:11 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@ pip install -r requirements.txt
 
 python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
-#python3.9 manage.py migrate --noinput
+##python3.9 manage.py migrate --noinput
 
 # Run collectstatic
 
