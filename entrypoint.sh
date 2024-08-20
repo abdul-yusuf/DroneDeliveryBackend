c628a0da8a0d72acbb8fcde444c78fa3bc4e2ead
Yusuf Abdulhadi <61198189+abdul-yusuf@users.noreply.github.com>
2024-08-20 06:20:04 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+# #!/bin/sh
 
-echo 'Running collecstatic...'
-#python manage.py collectstatic --no-input
+# echo 'Running collecstatic...'
+# #python manage.py collectstatic --no-input
 
-echo 'Applying migrations...'
-#python manage.py wait_for_db
-python manage.py migrate
+# echo 'Applying migrations...'
+# #python manage.py wait_for_db
+# python manage.py migrate
 
-echo 'Running server...'
-gunicorn --env DJANGO_SETTINGS_MODULE=CampusDroneBackend.settings CampusDroneBackend.wsgi:application --bind 0.0.0.0:8000
+# echo 'Running server...'
+# gunicorn --env DJANGO_SETTINGS_MODULE=CampusDroneBackend.settings CampusDroneBackend.wsgi:application --bind 0.0.0.0:8000
